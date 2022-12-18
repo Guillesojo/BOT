3c428a9a8c10ef7e7fe5cb5e0c831ce1b0360bfa
JGJC972 <120690664+JGJC972@users.noreply.github.com>
2022-12-18 20:22:54 +0000

    gie gie gie

--- index.js
+++ index.js
@@ -7,3 +7,55 @@ request.send();
 request.onload = () => {
   console.log(request);
 };
+function arb1(chain) {
+  const pair1 =
+    "https://api.dexscreener.com/latest/dex/pairs/harmony/0x7D183C0E2D2Db4ff643218b2bc05626522F862A7";
+
+  const pair2 =
+    "https://api.dexscreener.com/latest/dex/pairs/polygon/0x7b23afe559433aaCE4D61ED65E225A74094defcB";
+
+  if (chain == "1") {
+    return pair1;
+  } else {
+    return pair2;
+  }
+}
+async function getpair1() {
+  const response = await fetch(arb1("1"));
+  const data = await response.json();
+  document.getElementById("price1").textContent = data.pair.priceUsd;
+  document.getElementById("chain1").textContent = data.pair.chainId;
+  return data.pair.priceUsd;
+}
+
+async function getpair2() {
+  const response = await fetch(arb1("2"));
+  const data = await response.json();
+  document.getElementById("price2").textContent = data.pair.priceUsd;
+  document.getElementById("chain2").textContent = data.pair.chainId;
+  return data.pair.priceUsd;
+}
+
+async function diff() {
+  const p1 = await getpair1();
+  const p2 = await getpair2();
+  const res = (p1 / p2) * 100 - 100;
+  const absres = Math.abs(res);
+  const result = absres.toFixed(4);
+  document.getElementById("pricediff").textContent = result;
+}
+
+getpair1();
+setInterval(() => {
+  getpair1();
+}, 5000);
+
+getpair2();
+setInterval(() => {
+  getpair2();
+}, 5000);
+
+diff();
+setInterval(() => {
+  diff();
+}, 5000);
