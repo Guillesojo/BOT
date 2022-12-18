be1f8cc72df0f1f922046cdf1a96c02116994b0b
your_username <guilleedusj@gmail.com>
2022-12-17 21:15:25 -0600

    tests

--- index.js
+++ index.js
@@ -33,13 +33,3 @@ getpair2();
 setInterval(() => {
   getpair2();
 }, 5000);
-
-const request = new XMLHttpRequest();
-request.open(
-  "GET",
-  "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
-);
-request.send();
-request.onload = () => {
-  console.log(request);
-};
