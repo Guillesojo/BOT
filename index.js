90fe09ab0b2d85373d7b6bc552fe150a4fdd2109
JGJC972 <120690664+JGJC972@users.noreply.github.com>
2022-12-19 01:16:14 +0000

    backup

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-// newFunction();
-// function newFunction() {
-//   const request = new XMLHttpRequest();
-//   request.open(
-//     "GET",
-//     "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
-//   );
-//   request.send();
-//   request.onload = () => {
-//     console.log(request);
-//   };
-// }
-
+const request = new XMLHttpRequest();
+request.open(
+  "GET",
+  "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
+);
+request.send();
+request.onload = () => {
+  console.log(request);
+};
 function arb1(chain) {
   const pair1 =
     "https://api.dexscreener.com/latest/dex/pairs/harmony/0x7D183C0E2D2Db4ff643218b2bc05626522F862A7";
@@ -24,20 +20,6 @@ function arb1(chain) {
     return pair2;
   }
 }
-function screeners() {
-  var link1 = document.createElement("a");
-  link1.textContent = "screener";
-  link1.href =
-    "https://dexscreener.com/harmony/0x7d183c0e2d2db4ff643218b2bc05626522f862a7";
-  document.getElementById("scr1").appendChild(link1);
-
-  var link2 = document.createElement("a");
-  link2.textContent = "screener";
-  link2.href =
-    "https://dexscreener.com/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb";
-  document.getElementById("scr2").appendChild(link2);
-}
-
 async function getpair1() {
   const response = await fetch(arb1("1"));
   const data = await response.json();
@@ -75,5 +57,5 @@ setInterval(() => {
 
 diff();
 setInterval(() => {
-  diff(); HEAD
+  diff();
 }, 2000);
