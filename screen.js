04a46e7fd371fea118176aa860d6ed44f57645af
JGJC972 <120690664+JGJC972@users.noreply.github.com>
2022-12-24 19:17:30 +0000

    a

--- screen.js
+++ screen.js
@@ -35,6 +35,36 @@ var screens = [
   //12
   "https://dexscreener.com/polygon/0x76bba00e6d8b037821538f61963e6922e17c98a8",
   "https://dexscreener.com/avalanche/0x4d8bc902e9e6946f7e42aff2abdd24890d02ef0b",
+  //13
+  "https://dexscreener.com/arbitrum/0x6716ed27f13161ce3374f0d2cc8cef181681e5eb",
+  "https://dexscreener.com/bsc/0xeb80530dbbdf489e7a20175ceaa80d4253062dd4",
+  //14
+  "https://dexscreener.com/polygon/0x034293f21f1cce5908bc605ce5850df2b1059ac0",
+  "https://dexscreener.com/bsc/0xf65c1c0478efde3c19b49ecbe7acc57bb6b1d713",
+  //15
+  "https://dexscreener.com/polygon/0x38fe052f0ce76a2239115589098d2fb5aba01d80",
+  "https://dexscreener.com/dogechain/0xcce4baa8e36141fea5cff847f0f81fcf55b178ef",
+  //16
+  "https://dexscreener.com/polygon/0xf7abb7dee889da4300a82c26f8c0c725c64bd493",
+  "https://dexscreener.com/bsc/0x7062326862fc74d8731deca1d95ca1418896d67c",
+  //17
+  "https://dexscreener.com/polygon/0x8bab87ecf28bf45507bd745bc70532e968b5c2de",
+  "https://dexscreener.com/bsc/0xa5ac78a856345ab1fc8a4550208f42ca34b54e56",
+  //18
+  "https://dexscreener.com/fantom/0xa7010b3ba9efb1af9fa8a30efe74c16a93891775",
+  "https://dexscreener.com/bsc/0x89ee0491ce55d2f7472a97602a95426216167189",
+  //19
+  "https://dexscreener.com/polygon/0x55e49f32fbba12aa360eec55200dafd1ac47aaed",
+  "https://dexscreener.com/bsc/0xf1686f87d4c7bc935651f694492edb87f208a6be",
+  //20
+  "https://dexscreener.com/cronos/0x5383202d48c24aaa19873366168f2ed558a00ff0",
+  "https://dexscreener.com/arbitrum/0x2f5e87c9312fa29aed5c179e456625d79015299c",
+  //21
+  "https://dexscreener.com/polygon/0xb556fed3b348634a9a010374c406824ae93f0cf8",
+  "https://dexscreener.com/fantom/0x6badcf8184a760326528b11057c00952811f77af",
+  //22
+  "https://dexscreener.com/bsc/0x13321acff4a27f3d2bca64b8beac6e5fdaaaf12c",
+  "https://dexscreener.com/polygon/0xf67de5cf1fb01dc4df842a846df2a7ec07c41b93",
 ];
 
 function createLinks() {
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c3e7c56eb97a0679756e8827e15f6a92297ccd68',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c3e7c56eb97a0679756e8827e15f6a92297ccd68',
+    ),
+  ),
+);
