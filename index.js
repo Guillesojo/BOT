738c45d9a93e5e518338790884970bf92bd00a39
your_username <guilleedusj@gmail.com>
2022-12-23 20:48:01 -0600

    v0.3

--- index.js
+++ index.js
@@ -1,350 +1,113 @@
-async function arb1() {
-  const pair1 =
-    "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0xc73d2191a1dd0a99b377272899a5569ed83f8cd8";
+const AP1 = [
+  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0xc73d2191a1dd0a99b377272899a5569ed83f8cd8",
+  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0x4ecbc437911dce221b5f885813caa2c93ca64094",
+];
 
-  const pair2 =
-    "https://api.dexscreener.com/latest/dex/pairs/polygon/0x1dab41a0e410c25857f0f49b2244cd089ab88de6";
+const AP2 = [
+  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x1dab41a0e410c25857f0f49b2244cd089ab88de6",
+  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x0297e37f1873d2dab4487aa67cd56b58e2f27875000100000000000000000002-0x2791bca1f2de4661ed88a30c99a7a9449aa84174-0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3",
+];
 
+async function arbs() {
   async function getpair1() {
-    const response = await fetch(pair1);
-    const data = await response.json();
-    document.getElementById("ticker1").textContent = data.pair.baseToken.symbol;
-    document.getElementById("price1").textContent = data.pair.priceUsd;
-    document.getElementById("chain1").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function getpair2() {
-    const response = await fetch(pair2);
-    const data = await response.json();
-    document.getElementById("ticker2").textContent = data.pair.baseToken.symbol;
-    document.getElementById("price2").textContent = data.pair.priceUsd;
-    document.getElementById("chain2").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function diff() {
-    const p1 = await getpair1();
-    const p2 = await getpair2();
-    const res = (p1 / p2) * 100 - 100;
-    const absres = Math.abs(res);
-    const result = absres.toFixed(4);
-    document.getElementById("pricediff1").textContent = result;
+    const prices = [];
+    for (let i = 0; i < AP1.length; i++) {
+      const response = await fetch(AP1[i]);
+      const data = await response.json();
+      document.getElementById("ticker1-" + (i + 1)).textContent =
+        data.pair.baseToken.symbol;
+      document.getElementById("price1-" + (i + 1)).textContent =
+        data.pair.priceUsd;
+      document.getElementById("chain1-" + (i + 1)).textContent =
+        data.pair.chainId;
+      prices.push(data.pair.priceUsd);
+    }
+    return prices;
   }
   getpair1();
+
+  async function getpair2() {
+    const prices = [];
+    for (let i = 0; i < AP2.length; i++) {
+      const response = await fetch(AP2[i]);
+      const data = await response.json();
+      document.getElementById("ticker2-" + (i + 1)).textContent =
+        data.pair.baseToken.symbol;
+      document.getElementById("price2-" + (i + 1)).textContent =
+        data.pair.priceUsd;
+      document.getElementById("chain2-" + (i + 1)).textContent =
+        data.pair.chainId;
+      prices.push(data.pair.priceUsd);
+    }
+    return prices;
+  }
   getpair2();
+
+  async function diff() {
+    const price1 = await getpair1();
+    const price2 = await getpair2();
+    for (var i = 0; i < AP1.length; i++) {
+      const p1 = price1[i];
+      const p2 = price2[i];
+      const res = (p1 / p2) * 100 - 100;
+      const absres = Math.abs(res);
+      const result = absres.toFixed(4);
+      document.getElementById("pricediff" + (i + 1)).textContent = result;
+    }
+  }
   diff();
 }
 
-async function arb2() {
-  const pair1 =
-    "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0x4ecbc437911dce221b5f885813caa2c93ca64094";
-
-  const pair2 =
-    "https://api.dexscreener.com/latest/dex/pairs/polygon/0x0297e37f1873d2dab4487aa67cd56b58e2f27875000100000000000000000002-0x2791bca1f2de4661ed88a30c99a7a9449aa84174-0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3";
-
-  async function getpair1() {
-    const response = await fetch(pair1);
-    const data = await response.json();
-    document.getElementById("ticker3").textContent = data.pair.baseToken.symbol;
-    document.getElementById("price3").textContent = data.pair.priceUsd;
-    document.getElementById("chain3").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function getpair2() {
-    const response = await fetch(pair2);
-    const data = await response.json();
-    document.getElementById("ticker4").textContent = data.pair.baseToken.symbol;
-    document.getElementById("price4").textContent = data.pair.priceUsd;
-    document.getElementById("chain4").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function diff() {
-    const p1 = await getpair1();
-    const p2 = await getpair2();
-    const res = (p1 / p2) * 100 - 100;
-    const absres = Math.abs(res);
-    const result = absres.toFixed(4);
-    document.getElementById("pricediff2").textContent = result;
-  }
-  getpair1();
-  getpair2();
-  diff();
-}
-
-async function arb3() {
-  const pair1 =
-    "https://api.dexscreener.com/latest/dex/pairs/bsc/0x540ebc5c92839c300cb64d8350811aeee0c2b91d";
-
-  const pair2 =
-    "https://api.dexscreener.com/latest/dex/pairs/polygon/0x54db9acc40fd2ce8048fc36330502eedcecb71ba";
-
-  async function getpair1() {
-    const response = await fetch(pair1);
-    const data = await response.json();
-    document.getElementById("ticker5").textContent = data.pair.baseToken.symbol;
-    document.getElementById("price5").textContent = data.pair.priceUsd;
-    document.getElementById("chain5").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function getpair2() {
-    const response = await fetch(pair2);
-    const data = await response.json();
-    document.getElementById("ticker6").textContent = data.pair.baseToken.symbol;
-    document.getElementById("price6").textContent = data.pair.priceUsd;
-    document.getElementById("chain6").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function diff() {
-    const p1 = await getpair1();
-    const p2 = await getpair2();
-    const res = (p1 / p2) * 100 - 100;
-    const absres = Math.abs(res);
-    const result = absres.toFixed(4);
-    document.getElementById("pricediff3").textContent = result;
-  }
-  getpair1();
-  getpair2();
-  diff();
-}
-
-async function arb4() {
-  const pair1 =
-    "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x86783a149fe417831ae8c59dd0e2b60664a3dfd1";
-
-  const pair2 =
-    "https://api.dexscreener.com/latest/dex/pairs/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb";
-
-  async function getpair1() {
-    const response = await fetch(pair1);
-    const data = await response.json();
-    document.getElementById("ticker7").textContent = data.pair.baseToken.symbol;
-    document.getElementById("price7").textContent = data.pair.priceUsd;
-    document.getElementById("chain7").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function getpair2() {
-    const response = await fetch(pair2);
-    const data = await response.json();
-    document.getElementById("ticker8").textContent = data.pair.baseToken.symbol;
-    document.getElementById("price8").textContent = data.pair.priceUsd;
-    document.getElementById("chain8").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function diff() {
-    const p1 = await getpair1();
-    const p2 = await getpair2();
-    const res = (p1 / p2) * 100 - 100;
-    const absres = Math.abs(res);
-    const result = absres.toFixed(4);
-    document.getElementById("pricediff4").textContent = result;
-  }
-  getpair1();
-  getpair2();
-  diff();
-}
-
-async function arb5() {
-  const pair1 =
-    "https://api.dexscreener.com/latest/dex/pairs/harmony/0x7d183c0e2d2db4ff643218b2bc05626522f862a7";
-
-  const pair2 =
-    "https://api.dexscreener.com/latest/dex/pairs/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb";
-
-  async function getpair1() {
-    const response = await fetch(pair1);
-    const data = await response.json();
-    document.getElementById("ticker9").textContent = data.pair.baseToken.symbol;
-    document.getElementById("price9").textContent = data.pair.priceUsd;
-    document.getElementById("chain9").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function getpair2() {
-    const response = await fetch(pair2);
-    const data = await response.json();
-    document.getElementById("ticker10").textContent =
-      data.pair.baseToken.symbol;
-    document.getElementById("price10").textContent = data.pair.priceUsd;
-    document.getElementById("chain10").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function diff() {
-    const p1 = await getpair1();
-    const p2 = await getpair2();
-    const res = (p1 / p2) * 100 - 100;
-    const absres = Math.abs(res);
-    const result = absres.toFixed(4);
-    document.getElementById("pricediff5").textContent = result;
-  }
-  getpair1();
-  getpair2();
-  diff();
-}
-
-async function arb6() {
-  const pair1 =
-    "https://api.dexscreener.com/latest/dex/pairs/bsc/0x1955b7bf1ef1ecc5385bbcb2c13d529b035c6987";
-
-  const pair2 =
-    "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x68fb75a9488e405630fb6a4d7cce7d194e295d31";
-
-  async function getpair1() {
-    const response = await fetch(pair1);
-    const data = await response.json();
-    document.getElementById("ticker11").textContent =
-      data.pair.baseToken.symbol;
-    document.getElementById("price11").textContent = data.pair.priceUsd;
-    document.getElementById("chain11").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function getpair2() {
-    const response = await fetch(pair2);
-    const data = await response.json();
-    document.getElementById("ticker12").textContent =
-      data.pair.baseToken.symbol;
-    document.getElementById("price12").textContent = data.pair.priceUsd;
-    document.getElementById("chain12").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function diff() {
-    const p1 = await getpair1();
-    const p2 = await getpair2();
-    const res = (p1 / p2) * 100 - 100;
-    const absres = Math.abs(res);
-    const result = absres.toFixed(4);
-    document.getElementById("pricediff6").textContent = result;
-  }
-  getpair1();
-  getpair2();
-  diff();
-}
-
-async function arb7() {
-  const pair1 =
-    "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0x8d1a0676f7f3c4d55c59a963ee710540e6a3deba";
-
-  const pair2 =
-    "https://api.dexscreener.com/latest/dex/pairs/polygon/0xcbd258f33b7a2705e8418708a4f615c43fedf23c";
-
-  async function getpair1() {
-    const response = await fetch(pair1);
-    const data = await response.json();
-    document.getElementById("ticker13").textContent =
-      data.pair.baseToken.symbol;
-    document.getElementById("price13").textContent = data.pair.priceUsd;
-    document.getElementById("chain13").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function getpair2() {
-    const response = await fetch(pair2);
-    const data = await response.json();
-    document.getElementById("ticker14").textContent =
-      data.pair.baseToken.symbol;
-    document.getElementById("price14").textContent = data.pair.priceUsd;
-    document.getElementById("chain14").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function diff() {
-    const p1 = await getpair1();
-    const p2 = await getpair2();
-    const res = (p1 / p2) * 100 - 100;
-    const absres = Math.abs(res);
-    const result = absres.toFixed(4);
-    document.getElementById("pricediff7").textContent = result;
-  }
-  getpair1();
-  getpair2();
-  diff();
-}
-
-async function arb8() {
-  const pair1 =
-    "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x518008472ab60b4450f9077ab6fcf72456613729";
-
-  const pair2 =
-    "https://api.dexscreener.com/latest/dex/pairs/polygon/0xcbd258f33b7a2705e8418708a4f615c43fedf23c";
-
-  async function getpair1() {
-    const response = await fetch(pair1);
-    const data = await response.json();
-    document.getElementById("ticker15").textContent =
-      data.pair.baseToken.symbol;
-    document.getElementById("price15").textContent = data.pair.priceUsd;
-    document.getElementById("chain15").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function getpair2() {
-    const response = await fetch(pair2);
-    const data = await response.json();
-    document.getElementById("ticker16").textContent =
-      data.pair.baseToken.symbol;
-    document.getElementById("price16").textContent = data.pair.priceUsd;
-    document.getElementById("chain16").textContent = data.pair.chainId;
-    return data.pair.priceUsd;
-  }
-
-  async function diff() {
-    const p1 = await getpair1();
-    const p2 = await getpair2();
-    const res = (p1 / p2) * 100 - 100;
-    const absres = Math.abs(res);
-    const result = absres.toFixed(4);
-    document.getElementById("pricediff8").textContent = result;
-  }
-  getpair1();
-  getpair2();
-  diff();
-}
-
-arb1();
+arbs();
 setInterval(() => {
-  arb1();
+  arbs();
 }, 5000);
 
-arb2();
-setInterval(() => {
-  arb1();
-}, 5000);
+// async function arb3() {
+//   const pair1 =
+//     "https://api.dexscreener.com/latest/dex/pairs/bsc/0x540ebc5c92839c300cb64d8350811aeee0c2b91d";
 
-arb3();
-setInterval(() => {
-  arb1();
-}, 5000);
+//   const pair2 =
+//     "https://api.dexscreener.com/latest/dex/pairs/polygon/0x54db9acc40fd2ce8048fc36330502eedcecb71ba";
 
-arb4();
-setInterval(() => {
-  arb1();
-}, 5000);
+// async function arb4() {
+//   const pair1 =
+//     "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x86783a149fe417831ae8c59dd0e2b60664a3dfd1";
 
-arb5();
-setInterval(() => {
-  arb1();
-}, 5000);
+//   const pair2 =
+//     "https://api.dexscreener.com/latest/dex/pairs/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb";
 
-arb6();
-setInterval(() => {
-  arb1();
-}, 5000);
+// async function arb5() {
+//   const pair1 =
+//     "https://api.dexscreener.com/latest/dex/pairs/harmony/0x7d183c0e2d2db4ff643218b2bc05626522f862a7";
 
-arb7();
-setInterval(() => {
-  arb1();
-}, 5000);
+//   const pair2 =
+//     "https://api.dexscreener.com/latest/dex/pairs/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb";
 
-arb8();
-setInterval(() => {
-  arb1();
-}, 5000);
+// async function arb6() {
+//   const pair1 =
+//     "https://api.dexscreener.com/latest/dex/pairs/bsc/0x1955b7bf1ef1ecc5385bbcb2c13d529b035c6987";
+
+//   const pair2 =
+//     "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x68fb75a9488e405630fb6a4d7cce7d194e295d31";
+
+// async function arb7() {
+//   const pair1 =
+//     "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0x8d1a0676f7f3c4d55c59a963ee710540e6a3deba";
+
+//   const pair2 =
+//     "https://api.dexscreener.com/latest/dex/pairs/polygon/0xcbd258f33b7a2705e8418708a4f615c43fedf23c";
+
+// async function arb8() {
+//   const pair1 =
+//     "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x518008472ab60b4450f9077ab6fcf72456613729";
+
+//   const pair2 =
+//     "https://api.dexscreener.com/latest/dex/pairs/polygon/0xcbd258f33b7a2705e8418708a4f615c43fedf23c";
+
+// async function arb9() {
+//   const pair1 =
+//     "https://api.dexscreener.com/latest/dex/pairs/bsc/0x62616fe5495c04487ea15739045d4284ee99fdea";
+
+//   const pair2 =
+//     "https://api.dexscreener.com/latest/dex/pairs/polygon/0xd6ab3f53b1d71837fc1d66b856eeba7b87157e50";
--- screen.js
+++ screen.js
@@ -1,214 +1,97 @@
-var screen1 =
-  "https://dexscreener.com/arbitrum/0xc73d2191a1dd0a99b377272899a5569ed83f8cd8";
+var screens = [
+  "https://dexscreener.com/arbitrum/0xc73d2191a1dd0a99b377272899a5569ed83f8cd8",
+  "https://dexscreener.com/polygon/0x1dab41a0e410c25857f0f49b2244cd089ab88de6",
+  "https://dexscreener.com/arbitrum/0x4ecbc437911dce221b5f885813caa2c93ca64094",
+  "https://dexscreener.com/polygon/0x0297e37f1873d2dab4487aa67cd56b58e2f27875000100000000000000000002-0x2791bca1f2de4661ed88a30c99a7a9449aa84174-0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3",
+  "https://dexscreener.com/bsc/0x540ebc5c92839c300cb64d8350811aeee0c2b91d",
+  "https://dexscreener.com/polygon/0x54db9acc40fd2ce8048fc36330502eedcecb71ba",
+  "https://dexscreener.com/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb",
+  "https://dexscreener.com/avalanche/0x86783a149fe417831ae8c59dd0e2b60664a3dfd1",
+  "https://dexscreener.com/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb",
+  "https://dexscreener.com/harmony/0x7d183c0e2d2db4ff643218b2bc05626522f862a7",
+  "https://dexscreener.com/bsc/0x1955b7bf1ef1ecc5385bbcb2c13d529b035c6987",
+  "https://dexscreener.com/avalanche/0x68fb75a9488e405630fb6a4d7cce7d194e295d31",
+  "https://dexscreener.com/polygon/0xcbd258f33b7a2705e8418708a4f615c43fedf23c",
+  "https://dexscreener.com/arbitrum/0x8d1a0676f7f3c4d55c59a963ee710540e6a3deba",
+  "https://dexscreener.com/avalanche/0x518008472ab60b4450f9077ab6fcf7245",
+];
 
-var screen2 =
-  "https://dexscreener.com/polygon/0x1dab41a0e410c25857f0f49b2244cd089ab88de6";
+// var screen4 =
+//   "https://dexscreener.com/polygon/0x0297e37f1873d2dab4487aa67cd56b58e2f27875000100000000000000000002-0x2791bca1f2de4661ed88a30c99a7a9449aa84174-0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3";
 
-var screen3 =
-  "https://dexscreener.com/arbitrum/0x4ecbc437911dce221b5f885813caa2c93ca64094";
+// var screen5 =
+//   "https://dexscreener.com/bsc/0x540ebc5c92839c300cb64d8350811aeee0c2b91d";
 
-var screen4 =
-  "https://dexscreener.com/polygon/0x0297e37f1873d2dab4487aa67cd56b58e2f27875000100000000000000000002-0x2791bca1f2de4661ed88a30c99a7a9449aa84174-0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3";
+// var screen6 =
+//   "https://dexscreener.com/polygon/0x54db9acc40fd2ce8048fc36330502eedcecb71ba";
 
-var screen5 =
-  "https://dexscreener.com/bsc/0x540ebc5c92839c300cb64d8350811aeee0c2b91d";
+// var screen8 =
+//   "https://dexscreener.com/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb";
 
-var screen6 =
-  "https://dexscreener.com/polygon/0x54db9acc40fd2ce8048fc36330502eedcecb71ba";
+// var screen7 =
+//   "https://dexscreener.com/avalanche/0x86783a149fe417831ae8c59dd0e2b60664a3dfd1";
 
-var screen7 =
-  "https://dexscreener.com/optimism/0xd6e5824b54f64ce6f1161210bc17eebffc77e031000100000000000000000006-0x4200000000000000000000000000000000000042-0x97513e975a7fa9072c72c92d8000b0db90b163c5";
+// var screen10 =
+//   "https://dexscreener.com/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb";
 
-var screen8 =
-  "https://dexscreener.com/fantom/0x03c6b3f09d2504606936b1a4decefad204687890000200000000000000000015-0x04068da6c83afcfa0e13ba15a6696662335d5b75-0xf24bcf4d1e507740041c9cfd2dddb29585adce1e";
+// var screen9 =
+//   "https://dexscreener.com/harmony/0x7d183c0e2d2db4ff643218b2bc05626522f862a7";
 
-var screen9 =
-  "https://dexscreener.com/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb";
+// var screen11 =
+//   "https://dexscreener.com/bsc/0x1955b7bf1ef1ecc5385bbcb2c13d529b035c6987";
 
-var screen10 =
-  "https://dexscreener.com/harmony/0x7d183c0e2d2db4ff643218b2bc05626522f862a7";
+// var screen12 =
+//   "https://dexscreener.com/avalanche/0x68fb75a9488e405630fb6a4d7cce7d194e295d31";
 
-var screen11 =
-  "https://dexscreener.com/bsc/0x1955b7bf1ef1ecc5385bbcb2c13d529b035c6987";
+// var screen14 =
+//   "https://dexscreener.com/polygon/0xcbd258f33b7a2705e8418708a4f615c43fedf23c";
 
-var screen12 =
-  "https://dexscreener.com/avalanche/0x68fb75a9488e405630fb6a4d7cce7d194e295d31";
+// var screen13 =
+//   "https://dexscreener.com/arbitrum/0x8d1a0676f7f3c4d55c59a963ee710540e6a3deba";
 
-var screen13 =
-  "https://dexscreener.com/bsc/0xfe4fc96966b6b5eb6f71561f83a49c68c3e9acab";
+// var screen15 =
+//   "https://dexscreener.com/avalanche/0x518008472ab60b4450f9077ab6fcf72456613729";
 
-var screen14 =
-  "https://dexscreener.com/polygon/0x0baf0f4561ceab10540ba067ef30e6642074095d";
+// var screen16 =
+//   "https://dexscreener.com/polygon/0xcbd258f33b7a2705e8418708a4f615c43fedf23c";
 
-var screen15 =
-  "https://dexscreener.com/bsc/0x72ba008b631d9fd5a8e8013023cb3c05e19a7ca9";
+// var screen17 =
+//   "https://dexscreener.com/bsc/0x62616fe5495c04487ea15739045d4284ee99fdea";
 
-var screen16 =
-  "https://dexscreener.com/polygon/0xfd0e242c95b271844bf6860d4bc0e3e136bc0f7c";
+// var screen18 =
+//   "https://dexscreener.com/polygon/0xd6ab3f53b1d71837fc1d66b856eeba7b87157e50";
 
-var screen17 =
-  "https://dexscreener.com/bsc/0x62616fe5495c04487ea15739045d4284ee99fdea";
+// var screen19 =
+//   "https://dexscreener.com/bsc/0xc560f42334edab3b56ca9f99a7dd788f4010a231";
 
-var screen18 =
-  "https://dexscreener.com/polygon/0xd6ab3f53b1d71837fc1d66b856eeba7b87157e50";
+// var screen20 =
+//   "https://dexscreener.com/polygon/0x622753917443b4c7e62ec7bd9933b2d16a7795e9";
 
-var screen19 =
-  "https://dexscreener.com/bsc/0xc560f42334edab3b56ca9f99a7dd788f4010a231";
+// var screen21 =
+//   "https://dexscreener.com/arbitrum/0xc73d2191a1dd0a99b377272899a5569ed83f8cd8";
 
-var screen20 =
-  "https://dexscreener.com/polygon/0x622753917443b4c7e62ec7bd9933b2d16a7795e9";
+// var screen22 =
+//   "https://dexscreener.com/polygon/0x1dab41a0e410c25857f0f49b2244cd089ab88de6";
 
-var screen21 =
-  "https://dexscreener.com/arbitrum/0xc73d2191a1dd0a99b377272899a5569ed83f8cd8";
+// var screen23 =
+//   "https://dexscreener.com/bsc/0x241684be091a90c08a99df68ca68e05d96e1de10";
 
-var screen22 =
-  "https://dexscreener.com/polygon/0x1dab41a0e410c25857f0f49b2244cd089ab88de6";
+// var screen24 =
+//   "https://dexscreener.com/polygon/0xcbf71c04148e5c463223f07a64a50f2df46b6cdc";
 
-var screen23 =
-  "https://dexscreener.com/bsc/0x241684be091a90c08a99df68ca68e05d96e1de10";
+// var screen25 =
+//   "https://dexscreener.com/polygon/0x76bba00e6d8b037821538f61963e6922e17c98a8";
 
-var screen24 =
-  "https://dexscreener.com/polygon/0xcbf71c04148e5c463223f07a64a50f2df46b6cdc";
+// var screen26 =
+//   "https://dexscreener.com/avalanche/0x4d8bc902e9e6946f7e42aff2abdd24890d02ef0b";
 
-var screen25 =
-  "https://dexscreener.com/polygon/0x76bba00e6d8b037821538f61963e6922e17c98a8";
-
-var screen26 =
-  "https://dexscreener.com/avalanche/0x4d8bc902e9e6946f7e42aff2abdd24890d02ef0b";
-
-function screeners() {
-  var link1 = document.createElement("a");
-  link1.textContent = "screener";
-  link1.href = screen1;
-  link1.target = "_blank";
-  document.getElementById("scr1").appendChild(link1);
-
-  var link2 = document.createElement("a");
-  link2.textContent = "screener";
-  link2.href = screen2;
-  link2.target = "_blank";
-  document.getElementById("scr2").appendChild(link2);
-
-  var link3 = document.createElement("a");
-  link3.textContent = "screener";
-  link3.href = screen3;
-  link3.target = "_blank";
-  document.getElementById("scr3").appendChild(link3);
-
-  var link4 = document.createElement("a");
-  link4.textContent = "screener";
-  link4.href = screen4;
-  link4.target = "_blank";
-  document.getElementById("scr4").appendChild(link4);
-
-  var link5 = document.createElement("a");
-  link5.textContent = "screener";
-  link5.href = screen5;
-  document.getElementById("scr5").appendChild(link5);
-
-  var link6 = document.createElement("a");
-  link6.textContent = "screener";
-  link6.href = screen6;
-  document.getElementById("scr6").appendChild(link6);
-
-  var link7 = document.createElement("a");
-  link7.textContent = "screener";
-  link7.href = screen7;
-  document.getElementById("scr7").appendChild(link7);
-
-  var link8 = document.createElement("a");
-  link8.textContent = "screener";
-  link8.href = screen8;
-  document.getElementById("scr8").appendChild(link8);
-
-  var link9 = document.createElement("a");
-  link9.textContent = "screener";
-  link9.href = screen9;
-  document.getElementById("scr9").appendChild(link9);
-
-  var link10 = document.createElement("a");
-  link10.textContent = "screener";
-  link10.href = screen10;
-  document.getElementById("scr10").appendChild(link10);
-
-  var link11 = document.createElement("a");
-  link11.textContent = "screener";
-  link11.href = screen11;
-  document.getElementById("scr11").appendChild(link11);
-
-  var link12 = document.createElement("a");
-  link12.textContent = "screener";
-  link12.href = screen12;
-  document.getElementById("scr12").appendChild(link12);
-
-  var link13 = document.createElement("a");
-  link13.textContent = "screener";
-  link13.href = screen13;
-  document.getElementById("scr13").appendChild(link13);
-
-  var link14 = document.createElement("a");
-  link14.textContent = "screener";
-  link14.href = screen14;
-  document.getElementById("scr14").appendChild(link14);
-
-  var link15 = document.createElement("a");
-  link15.textContent = "screener";
-  link15.href = screen15;
-  document.getElementById("scr15").appendChild(link15);
-
-  var link16 = document.createElement("a");
-  link16.textContent = "screener";
-  link16.href = screen16;
-  document.getElementById("scr16").appendChild(link16);
-
-  var link17 = document.createElement("a");
-  link17.textContent = "screener";
-  link17.href = screen17;
-  document.getElementById("scr17").appendChild(link17);
-
-  var link18 = document.createElement("a");
-  link18.textContent = "screener";
-  link18.href = screen18;
-  document.getElementById("scr18").appendChild(link18);
-
-  var link19 = document.createElement("a");
-  link19.textContent = "screener";
-  link19.href = screen19;
-  document.getElementById("scr19").appendChild(link19);
-
-  var link20 = document.createElement("a");
-  link20.textContent = "screener";
-  link20.href = screen20;
-  document.getElementById("scr20").appendChild(link20);
-
-  var link21 = document.createElement("a");
-  link21.textContent = "screener";
-  link21.href = screen21;
-  document.getElementById("scr21").appendChild(link21);
-
-  var link22 = document.createElement("a");
-  link22.textContent = "screener";
-  link22.href = screen22;
-  document.getElementById("scr22").appendChild(link22);
-
-  var link23 = document.createElement("a");
-  link23.textContent = "screener";
-  link23.href = screen23;
-  document.getElementById("scr23").appendChild(link23);
-
-  var link24 = document.createElement("a");
-  link24.textContent = "screener";
-  link24.href = screen24;
-  document.getElementById("scr24").appendChild(link24);
-
-  var link25 = document.createElement("a");
-  link25.textContent = "screener";
-  link25.href = screen25;
-  document.getElementById("scr25").appendChild(link25);
-
-  var link26 = document.createElement("a");
-  link26.textContent = "screener";
-  link26.href = screen26;
-  document.getElementById("scr26").appendChild(link26);
+function createLinks() {
+  for (var i = 0; i < screens.length; i++) {
+    var link = document.createElement("a");
+    link.textContent = "screener";
+    link.href = screens[i];
+    link.target = "_blank";
+    document.getElementById("scr" + (i + 1)).appendChild(link);
+  }
 }
-screeners();
+createLinks();
