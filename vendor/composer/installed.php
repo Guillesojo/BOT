01a39ac197899653039d52e8206a0e99213ccb51
JGJC972 <120690664+JGJC972@users.noreply.github.com>
2023-01-06 17:50:37 +0000

    L

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b5d417657e13004919f6f34226fafb8420a516f7',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b5d417657e13004919f6f34226fafb8420a516f7',
+    ),
+  ),
+);
