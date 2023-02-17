225e34867f0a9e44ebc976f209d28ce425d036f9
your_username <guilleedusj@gmail.com>
2023-02-16 21:56:25 -0600

    Optimizations

--- index.js
+++ index.js
@@ -1,169 +1,137 @@
-const AP1 = [
-  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0xc73d2191a1dd0a99b377272899a5569ed83f8cd8",
-  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0xcc65a812ce382ab909a11e434dbf75b34f1cc59d000200000000000000000001-0x040d1edc9569d4bab2d15287dc5a4f10f56a56b8-0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x540ebc5c92839c300cb64d8350811aeee0c2b91d",
-  "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x86783a149fe417831ae8c59dd0e2b60664a3dfd1",
-  "https://api.dexscreener.com/latest/dex/pairs/harmony/0x7d183c0e2d2db4ff643218b2bc05626522f862a7",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x1955b7bf1ef1ecc5385bbcb2c13d529b035c6987",
-  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0x8d1a0676f7f3c4d55c59a963ee710540e6a3deba",
-  "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x518008472ab60b4450f9077ab6fcf72456613729",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0xc560f42334edab3b56ca9f99a7dd788f4010a231",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x241684be091a90c08a99df68ca68e05d96e1de10",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x76bba00e6d8b037821538f61963e6922e17c98a8",
-  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0x6716ed27f13161ce3374f0d2cc8cef181681e5eb",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x034293f21f1cce5908bc605ce5850df2b1059ac0",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0xf7abb7dee889da4300a82c26f8c0c725c64bd493",
-  "https://api.dexscreener.com/latest/dex/pairs/fantom/0xa7010b3ba9efb1af9fa8a30efe74c16a93891775",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x55e49f32fbba12aa360eec55200dafd1ac47aaed",
-  "https://api.dexscreener.com/latest/dex/pairs/cronos/0x5383202d48c24aaa19873366168f2ed558a00ff0",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0xb556fed3b348634a9a010374c406824ae93f0cf8",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x13321acff4a27f3d2bca64b8beac6e5fdaaaf12c",
-  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0x6716ed27f13161ce3374f0d2cc8cef181681e5eb",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x561ed3fbeac3c4e5b060024666f9a1cd2aec7847",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb",
-  "https://api.dexscreener.com/latest/dex/pairs/cronos/0xb6894f071da907301b4f0c4d87d5f2e42e22402d",
-  "https://api.dexscreener.com/latest/dex/pairs/optimism/0xad4c666fc170b468b19988959eb931a3676f0e9f",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x79c38d6c5ea40e9dd82d004bff98e3ec707d2c3d",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x89c68051543fa135b31c2ce7bd8cdf392345ff01",
-
-  "https://api.dexscreener.com/latest/dex/pairs/cronos/0x0fbab8a90cac61b481530aad3a64fe17b322c25d",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0xadbd183ff04ff3dc9cd980f07b308c25df77e860",
-  "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x6a0c03c0b933875daf767bb90584ba696b713243",
-  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0x67d6cbf707fe5747989cb2c3758d101e11459ea9",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0xd0595ff44638182e6b54051493ec0f045d04335f",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0xab4b9f608f2b5e53b259b1878b68b93a1309578b",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x8205a8b42ee0bfdab43af3c971d97e150a1e48ae",
-];
-
-const AP2 = [
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x1dab41a0e410c25857f0f49b2244cd089ab88de6",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x0297e37f1873d2dab4487aa67cd56b58e2f27875000100000000000000000002-0x2791bca1f2de4661ed88a30c99a7a9449aa84174-0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x54db9acc40fd2ce8048fc36330502eedcecb71ba",
-  "https://api.dexscreener.com/latest/dex/pairs/harmony/0x7d183c0e2d2db4ff643218b2bc05626522f862a7",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x7b23afe559433aace4d61ed65e225a74094defcb",
-  "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x68fb75a9488e405630fb6a4d7cce7d194e295d31",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x77cdc7c61fa089e236dcc22427e37e7ea1adbfcf",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x77cdc7c61fa089e236dcc22427e37e7ea1adbfcf",
-  "https://api.dexscreener.com/latest/dex/pairs/avalanche/0xbf669420e8b49070f94f7cb04b4a5ecdcd0420c2",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0xcbf71c04148e5c463223f07a64a50f2df46b6cdc",
-  "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x4d8bc902e9e6946f7e42aff2abdd24890d02ef0b",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0xeb80530dbbdf489e7a20175ceaa80d4253062dd4",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0xf65c1c0478efde3c19b49ecbe7acc57bb6b1d713",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x7062326862fc74d8731deca1d95ca1418896d67c",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x89ee0491ce55d2f7472a97602a95426216167189",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0xf1686f87d4c7bc935651f694492edb87f208a6be",
-  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0x2f5e87c9312fa29aed5c179e456625d79015299c",
-  "https://api.dexscreener.com/latest/dex/pairs/fantom/0x6badcf8184a760326528b11057c00952811f77af",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0xf67de5cf1fb01dc4df842a846df2a7ec07c41b93",
-  "https://api.dexscreener.com/latest/dex/pairs/harmony/0x27f3b2df4a81382202e87ee40429e0212ecc7d3f",
-  "https://api.dexscreener.com/latest/dex/pairs/avalanche/0xb448a6772648da6d16ab0167484242e07abf644e",
-  "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x86783a149fe417831ae8c59dd0e2b60664a3dfd1",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0xab86c5dd50f4e0b54ecb07c4fb07219c60150ebf",
-  "https://api.dexscreener.com/latest/dex/pairs/arbitrum/0xc24f7d8e51a64dc1238880bd00bb961d54cbeb29",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0xad308b210356d69026c08c5f51089197d4bb59a6",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x5a6861566966654d0464ee5e3f3ec1a034f26cbc",
-  "https://api.dexscreener.com/latest/dex/pairs/fantom/0x1a2857b70d596eaa7182427c71cefd780a8f2aca",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x1766f21f6581ae7410dd7d481c384b2b70bcc42e",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x8cb83d4b4a406589b78664add7060a79430c077a",
-  "https://api.dexscreener.com/latest/dex/pairs/cronos/0x8a01774bf0622cb45b207bf2baf0d694300dac60",
-  "https://api.dexscreener.com/latest/dex/pairs/fantom/0x9baa8eea25522234bbe36e93133db34711d3e66b",
-  "https://api.dexscreener.com/latest/dex/pairs/bsc/0x41138b6466e1d92ae05495735d46586b74efad75",
-  "https://api.dexscreener.com/latest/dex/pairs/polygon/0x78b1644539bb233d1477667ebd51d9db6ed7abcc",
-];
-
 Notification.requestPermission();
 
+async function info() {
+  const requestURL = "info.json";
+  const request = new Request(requestURL);
+  const response = await fetch(request);
+  const list = await response.json();
+  return list;
+}
+
+function create() {
+  const rows = 23;
+  const container = document.getElementsByClassName("container-fluid")[0];
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+
+    for (let j = 0; j < 3; j++) {
+      const cell = document.createElement("div");
+      cell.className = "cell col-lg-4 col-xs";
+
+      const arb = document.createElement("div");
+      arb.id = `arb${i * 3 + j + 1}`;
+      arb.className = "arbitrage";
+
+      const card = document.createElement("div");
+      card.className = "card";
+
+      const cardBody = document.createElement("div");
+      cardBody.className = "card-body";
+
+      const p1 = document.createElement("p");
+      p1.innerHTML = `
+        <span id="tickerA${i * 3 + j + 1}"></span> <span id="chainA${
+        i * 3 + j + 1
+      }"></span>
+        <span id="priceA${i * 3 + j + 1}"></span>
+        <span id="scr${i * 6 + j * 2 + 1}" class="screener"></span>
+      `;
+      const p2 = document.createElement("p");
+      p2.innerHTML = `
+        <span id="tickerB${i * 3 + j + 1}"></span> <span id="chainB${
+        i * 3 + j + 1
+      }"></span>
+        <span id="priceB${i * 3 + j + 1}"></span>
+        <span id="scr${i * 6 + j * 2 + 2}" class="screener"></span>
+      `;
+
+      const p3 = document.createElement("p");
+      p3.className = "diff";
+      p3.innerHTML = `<span id="pricediff${i * 3 + j + 1}"></span>`;
+
+      cardBody.appendChild(p1);
+      cardBody.appendChild(p2);
+      cardBody.appendChild(p3);
+
+      card.appendChild(cardBody);
+      arb.appendChild(card);
+      cell.appendChild(arb);
+      row.appendChild(cell);
+    }
+
+    container.appendChild(row);
+  }
+}
+create();
+
 async function arbs() {
-  // Declaración de función  "getData" con dos parámetros: "urlList" y "callback"
-  //Llama a la API de Dexscreener y convierte la respuesta a JSON
-  async function getData(urlList, callback) {
-    // Constante "responses" que contiene el resultado de la promesa de ejecutar "fetch" en cada URL de la lista "urlList"
+  async function AP(offset) {
+    const list = await info();
+    const urls = [];
+    for (var i = offset; i < list.length; i += 2) {
+      const full = `https://api.dexscreener.com/latest/dex/pairs/${list[i]}`;
+      urls.push(full);
+    }
+    return urls;
+  }
+
+  async function getData(urlList) {
     const responses = await Promise.all(urlList.map((url) => fetch(url)));
-    // Constante "dataList" que contiene el resultado de la promesa de convertir cada respuesta en formato JSON
     const dataList = await Promise.all(
       responses.map((response) => response.json())
     );
-    // Constante "results" que contiene el resultado de aplicar la función "callback" a cada elemento de "dataList" y su índice
-    const results = dataList.map((data, i) => callback(data, i));
-    // Retorno de "results"
-    return results;
+    return dataList;
   }
 
-  // Declaración de función "getpair1" para obtener la info del primer par
-  async function getpair1() {
-    // Constante "prices" que contiene el resultado de la función "getData" con la lista de URLs "AP1" y una función "callback"
-    const prices = await getData(AP1, (data, i) => {
-      // Modificación del contenido del elemento HTML con id "tickerA" más el índice más 1
-      document.getElementById("tickerA" + (i + 1)).textContent =
-        data.pair.baseToken.symbol + " on";
-      // Modificación del contenido del elemento HTML con id "priceA" más el índice más 1
-      document.getElementById("priceA" + (i + 1)).textContent =
+  async function updatePairs(dataList, baseId, priceId, chainId) {
+    const price = [];
+    for (var i = 0; i < dataList.length; i++) {
+      const data = dataList[i];
+      document.getElementById(
+        `${baseId}${i + 1}`
+      ).textContent = `${data.pair.baseToken.symbol} on`;
+      document.getElementById(`${priceId}${i + 1}`).textContent =
         data.pair.priceUsd;
-      // Modificación del contenido del elemento HTML con id "chainA" más el índice más 1
-      document.getElementById("chainA" + (i + 1)).textContent =
-        data.pair.chainId + ": ";
-      // Retorno del valor de "priceUsd"
-      return data.pair.priceUsd;
-    });
-    // Retorno de "prices"
-    return prices;
+      document.getElementById(
+        `${chainId}${i + 1}`
+      ).textContent = `${data.pair.chainId}: `;
+      price.push(data.pair.priceUsd);
+    }
+    return price;
   }
 
-  // Declaración de función "getpair1" para obtener la info del segundo par
-  //Hace lo mismo que la función "getpair1", solo cambian los id's del HTML
-  async function getpair2() {
-    const prices = await getData(AP2, (data, i) => {
-      document.getElementById("tickerB" + (i + 1)).textContent =
-        data.pair.baseToken.symbol + " on";
+  const pair1Data = await getData(await AP(0));
+  const pair2Data = await getData(await AP(1));
+  console.log(updatePairs(pair1Data, "tickerA", "priceA", "chainA"));
+  // updatePairs(pair1Data, "tickerA", "priceA", "chainA");
+  // updatePairs(pair2Data, "tickerB", "priceB", "chainB");
 
-      document.getElementById("priceB" + (i + 1)).textContent =
-        data.pair.priceUsd;
-
-      document.getElementById("chainB" + (i + 1)).textContent =
-        data.pair.chainId + ": ";
-
-      return data.pair.priceUsd;
-    });
-
-    // Retorno de "prices"
-    return prices;
-  }
-
-  // Declaración de función "diff", que compara los dos precios y reotrna la diferencia
   async function diff() {
-    // Declaración de una lista vacía "resul"
     const resul = [];
-    // Constante "price1" que contiene el resultado de la función "getpair1"
-    const price1 = await getpair1();
-    // Constante "price2" que contiene el resultado de la función "getpair2"
-    const price2 = await getpair2();
-    // Iteración a través de la longitud de "AP1"
-    for (var i = 0; i < AP1.length; i++) {
-      // Constante "p1" que contiene el valor en la posición "i" de "price1"
+    const price1 = await updatePairs(pair1Data, "tickerA", "priceA", "chainA");
+    const price2 = await updatePairs(pair2Data, "tickerB", "priceB", "chainB");
+
+    const l = await updatePairs(pair1Data, "tickerA", "priceA", "chainA");
+    for (var i = 0; i < l.length; i++) {
       const p1 = price1[i];
-      // Constante "p2" que contiene el valor en la posición "i" de "price2"
       const p2 = price2[i];
-      // Constante "res" que contiene el resultado de dividir "p1" entre "p2" y multiplicarlo por 100 y restarle 100
       const res = (p1 / p2) * 100 - 100;
-      // Constante "absres" que contiene el valor absoluto de "res"
       const absres = Math.abs(res);
-      // Constante "result" que contiene el valor de "absres" con cuatro decimales
       const result = absres.toFixed(4);
-      // Modificación del contenido del elemento HTML con id "pricediff" más el índice más 1
       document.getElementById("pricediff" + (i + 1)).textContent = result + "%";
-      // Añadir "result" a la lista "resul"
       resul.push(result);
     }
-    // Retorno de "resul"
+
     return resul;
   }
+
   async function identify() {
     const results = await diff();
-    console.log(results);
-    const ticker = await getData(AP1, (data, i) => {
+
+    const ticker = await getData(await AP(0), (data, i) => {
       return data.pair.baseToken.symbol;
     });
-    console.log(ticker);
     for (var i = 0; i < results.length; i++) {
       if (results[i] >= 5) {
         document.getElementById("pricediff" + (i + 1)).style.color = "green";
@@ -173,11 +141,25 @@ async function arbs() {
       }
     }
   }
+
   identify();
 }
 
+async function createLinks() {
+  list = await info();
+  for (var i = 0; i < list.length; i++) {
+    screener = "https://dexscreener.com/" + list[i];
+    var link = document.createElement("a");
+    link.textContent = "screener";
+    link.href = screener;
+    link.target = "_blank";
+    document.getElementById("scr" + (i + 1)).appendChild(link);
+  }
+}
+createLinks();
+
 arbs();
-// Ejecución cada 30 segundos de las funciones "arbs" y "identify"
+// Ejecución cada 30 segundos de las funciones "arbs"
 setInterval(() => {
   arbs();
-}, 30000);
+}, 60000);
